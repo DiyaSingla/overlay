9b3756122613cf62d6516202816f5c7d0920d7eb
Diya <diyasingla2004@gmail.com>
2024-09-15 17:43:17 +0530

    Initial commit

---
+++ lib/overlays/true_caller_overlay.dart
@@ -0,0 +1,128 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:flutter_overlay_window/flutter_overlay_window.dart';
+
+class HealthOverlay extends StatefulWidget {
+  final String detectedText;
+
+  const HealthOverlay({Key? key, required this.detectedText}) : super(key: key);
+
+  @override
+  State<HealthOverlay> createState() => _HealthOverlayState();
+}
+
+class _HealthOverlayState extends State<HealthOverlay> {
+  bool isGold = true;
+
+  final _goldColors = const [
+    Color(0xFF055b49),
+    Color(0xFFebd197),
+    Color(0xFFa2790d),
+  ];
+
+  final _silverColors = const [
+    Color(0xFFAEB2B8),
+    Color(0xFFC7C9CB),
+    Color(0xFFD7D7D8),
+    Color(0xFFAEB2B8),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 12.0),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: isGold ? _goldColors : _silverColors,
+            ),
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                isGold = !isGold;
+              });
+              FlutterOverlayWindow.getOverlayPosition().then((value) {
+                log("Overlay Position: $value");
+              });
+            },
+            child: Stack(
+              children: [
+                Column(
+                  children: [
+                    ListTile(
+                      leading: Container(
+                        height: 80.0,
+                        width: 80.0,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black54),
+                          shape: BoxShape.circle,
+                          image: const DecorationImage(
+                            image: NetworkImage(
+                                "https://api.multiavatar.com/x-slayer.png"),
+                          ),
+                        ),
+                      ),
+                      title: const Text(
+                        "Analysis",
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(widget.detectedText),
+                    ),
+                    const Spacer(),
+                    const Divider(color: Colors.black54),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("High Sugar, HIgh Saturated Fats"),
+                              Text("Rich in Vitamins"),
+                            ],
+                          ),
+                          Text(
+                            "2.5/5",
+                            style: TextStyle(
+                                fontSize: 15.0, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: IconButton(
+                    onPressed: () async {
+                      await FlutterOverlayWindow.closeOverlay();
+                    },
+                    icon: const Icon(
+                      Icons.close,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
